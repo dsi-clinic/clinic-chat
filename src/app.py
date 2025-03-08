1c6d9d983a078dcdd3c261c97bf4cd70c4647122
Tim Hannifan <hannifan@gmail.com>
2025-03-08 15:32:09 -0600

    adding pills to frontend

--- src/app.py
+++ src/app.py
@@ -8,30 +8,27 @@ import openai
 import streamlit as st
 from llama_index.core import Settings, SimpleDirectoryReader, VectorStoreIndex
 from llama_index.llms.openai import OpenAI
+from streamlit_pills import pills
 
 st.set_page_config(
     page_title="Clinic Chat, powered by LlamaIndex",
-    page_icon="🦙",
+    page_icon="./img/avatar.png",
     layout="centered",
     initial_sidebar_state="auto",
     menu_items=None,
 )
 openai.api_key = st.secrets.OPENAI_API_KEY
-st.image("img/clinic.png", width=200)
-# st.title(
-#     "Chat with the Data Science Clinic's GitHub page, powered by LlamaIndex 💬🦙"
-# )
 
-if (
-    "messages" not in st.session_state.keys()
-):  # Initialize the chat messages history
+st.image("img/clinic.png", width=200)
+
+# Initialize the chat messages history
+if "messages" not in st.session_state:  
     st.session_state.messages = [
         {
             "role": "assistant",
             "content": "Ask me a question about the Data Science Clinic!",
         }
     ]
-
 
 def download_repo(repo_url, repo_path):
     """Download a repo"""
@@ -67,17 +64,30 @@ def load_data():
 
 index = load_data()
 
-if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
+# Initialize the chat engine
+if "chat_engine" not in st.session_state.keys():
     st.session_state.chat_engine = index.as_chat_engine(
         chat_mode="condense_question", verbose=True, streaming=True
     )
 
-if prompt := st.chat_input(
-    "Ask a question"
-):  # Prompt for user input and save to chat history
+selected = pills(
+    "Choose a question to get started or write your own below.",
+    [
+        "When are the final deliverables due?",
+        "Where is the application?",
+        "How do I set up my computer?",
+        "What are the one-pager requirements?"
+    ],
+    clearable=False,
+    index=None,
+)
+
+# Prompt for user input and save to chat history
+if prompt := st.chat_input("Ask a question"):  
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-for message in st.session_state.messages:  # Write message history to UI
+# Write message history to UI
+for message in st.session_state.messages:
     # If message is from assistant, write it to the UI as a chat message
     if message["role"] == "assistant":
         with st.chat_message(message["role"], avatar="./img/avatar.png"):
@@ -85,6 +95,20 @@ for message in st.session_state.messages:  # Write message history to UI
     else:
         with st.chat_message(message["role"]):
             st.write(message["content"])
+
+# To avoid duplicated display of answered pill questions each rerun
+if selected and selected not in st.session_state.get(
+    "displayed_pill_questions", set()
+):
+    st.session_state.setdefault("displayed_pill_questions", set()).add(selected)
+
+    with st.chat_message("user"):
+        st.write(selected)
+        user_message = {"role": "user", "content": selected}
+        st.session_state.messages.append(user_message)
+
+    st.session_state["chat_engine"].stream_chat(selected)
+
 # If last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant", avatar="./img/avatar.png"):
